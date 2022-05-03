1f4f300e46c4f8036c4c6652a980c2fd1bbc8778
me.saif <me.shaikhsaif@gmail.com>
2022-05-03 11:21:54 +0530

    Add Categories Controller for Ajax Call

---
+++ Blogs/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using Blogs.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blogs.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly BlogDbContext _context;
+
+        public CategoriesController(BlogDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetCategories()
+        {
+            var categories = _context.Categories.ToList();
+            return new JsonResult(Ok(categories));
+        }
+    }
+}
